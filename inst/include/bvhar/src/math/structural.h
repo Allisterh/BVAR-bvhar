e900a56f820a1273a0344b1968416297a65543b4
ygeunkim <ygeunkimstat.gmail.com>
2025-05-24 13:33:44 +0900

    replace block with topRows and middleRows

--- inst/include/bvhar/src/math/structural.h
+++ inst/include/bvhar/src/math/structural.h
@@ -55,7 +55,7 @@ inline bool is_stable(Eigen::Ref<const Eigen::MatrixXd> coef_mat, double thresho
 	return stableroot.maxCoeff() < threshold;
 }
 
-inline Eigen::MatrixXd convert_var_to_vma(Eigen::MatrixXd var_coef, int var_lag, int lag_max) {
+inline Eigen::MatrixXd convert_var_to_vma(Eigen::Ref<Eigen::MatrixXd> var_coef, int var_lag, int lag_max) {
   int dim = var_coef.cols(); // m
   if (lag_max < 1) {
     STOP("'lag_max' must larger than 0");
@@ -66,23 +66,23 @@ inline Eigen::MatrixXd convert_var_to_vma(Eigen::MatrixXd var_coef, int var_lag,
     num_full_arows = dim * var_lag; // for VMA coefficient q < VAR(p)
   }
   Eigen::MatrixXd FullA = Eigen::MatrixXd::Zero(num_full_arows, dim); // same size with VMA coefficient matrix
-  FullA.block(0, 0, dim * var_lag, dim) = var_coef.block(0, 0, dim * var_lag, dim); // fill first mp row with VAR coefficient matrix
-  Eigen::MatrixXd Im = Eigen::MatrixXd::Identity(dim, dim); // identity matrix
+  FullA.topRows(dim * var_lag) = var_coef.topRows(dim * var_lag); // fill first mp row with VAR coefficient matrix
+  // Eigen::MatrixXd Im = Eigen::MatrixXd::Identity(dim, dim); // identity matrix
   Eigen::MatrixXd ma = Eigen::MatrixXd::Zero(ma_rows, dim); // VMA [W1^T, W2^T, ..., W(lag_max)^T]^T, ma_rows = m * lag_max
-  ma.block(0, 0, dim, dim) = Im; // W0 = Im
-  ma.block(dim, 0, dim, dim) = FullA.block(0, 0, dim, dim) * ma.block(0, 0, dim, dim); // W1^T = B1^T * W1^T
+  ma.topRows(dim) = Eigen::MatrixXd::Identity(dim, dim); // W0 = Im
+  ma.middleRows(dim, dim) = FullA.topRows(dim) * ma.topRows(dim); // W1^T = B1^T * W1^T
   if (lag_max == 1) {
     return ma;
   }
   for (int i = 2; i < (lag_max + 1); i++) { // from W2: m-th row
     for (int k = 0; k < i; k++) {
-      ma.block(i * dim, 0, dim, dim) += FullA.block(k * dim, 0, dim, dim) * ma.block((i - k - 1) * dim, 0, dim, dim); // Wi = sum(W(i - k)^T * Bk^T)
+      ma.middleRows(i * dim, dim) += FullA.middleRows(k * dim, dim) * ma.middleRows((i - k - 1) * dim, dim); // Wi = sum(W(i - k)^T * Bk^T)
     }
   }
   return ma;
 }
 
-inline Eigen::MatrixXd convert_vma_ortho(Eigen::MatrixXd var_coef, Eigen::MatrixXd var_covmat, int var_lag, int lag_max) {
+inline Eigen::MatrixXd convert_vma_ortho(Eigen::Ref<Eigen::MatrixXd> var_coef, Eigen::Ref<Eigen::MatrixXd> var_covmat, int var_lag, int lag_max) {
   int dim = var_covmat.cols(); // num_rows = num_cols
   if ((dim != var_covmat.rows()) && (dim != var_coef.cols())) {
     STOP("Wrong covariance matrix format: `var_covmat`.");
@@ -95,25 +95,25 @@ inline Eigen::MatrixXd convert_vma_ortho(Eigen::MatrixXd var_coef, Eigen::Matrix
   Eigen::LLT<Eigen::MatrixXd> lltOfcovmat(Eigen::Map<Eigen::MatrixXd>(var_covmat.data(), dim, dim)); // cholesky decomposition for Sigma
   Eigen::MatrixXd chol_covmat = lltOfcovmat.matrixU();
   for (int i = 0; i < lag_max + 1; i++) {
-    res.block(i * dim, 0, dim, dim) = chol_covmat * ma.block(i * dim, 0, dim, dim);
+    res.middleRows(i * dim, dim) = chol_covmat * ma.middleRows(i * dim, dim);
   }
   return res;
 }
 
-inline Eigen::MatrixXd compute_var_mse(const Eigen::MatrixXd& cov_mat, const Eigen::MatrixXd& var_coef,
+inline Eigen::MatrixXd compute_var_mse(Eigen::Ref<Eigen::MatrixXd> cov_mat, Eigen::Ref<Eigen::MatrixXd> var_coef,
 																			 int var_lag, int step) {
 	int dim = cov_mat.cols(); // dimension of time series
   Eigen::MatrixXd vma_mat = convert_var_to_vma(var_coef, var_lag, step);
   Eigen::MatrixXd innov_account = Eigen::MatrixXd::Zero(dim, dim);
   Eigen::MatrixXd mse = Eigen::MatrixXd::Zero(dim * step, dim);
   for (int i = 0; i < step; i++) {
-    innov_account += vma_mat.block(i * dim, 0, dim, dim).transpose() * cov_mat * vma_mat.block(i * dim, 0, dim, dim);
+    innov_account += vma_mat.middleRows(i * dim, dim).transpose() * cov_mat * vma_mat.middleRows(i * dim, dim);
     mse.block(i * dim, 0, dim, dim) = innov_account;
   }
   return mse;
 }
 
-inline Eigen::MatrixXd convert_vhar_to_vma(Eigen::MatrixXd vhar_coef, Eigen::MatrixXd HARtrans_mat, int lag_max, int month) {
+inline Eigen::MatrixXd convert_vhar_to_vma(Eigen::Ref<Eigen::MatrixXd> vhar_coef, Eigen::Ref<Eigen::MatrixXd> HARtrans_mat, int lag_max, int month) {
   int dim = vhar_coef.cols(); // dimension of time series
   // Eigen::MatrixXd coef_mat = HARtrans_mat.transpose() * vhar_coef; // bhat = tilde(T)^T * Phi
 	Eigen::MatrixXd coef_mat = harx_to_var(vhar_coef, HARtrans_mat);
@@ -125,20 +125,22 @@ inline Eigen::MatrixXd convert_vhar_to_vma(Eigen::MatrixXd vhar_coef, Eigen::Mat
   if (lag_max < month) num_full_arows = month * dim; // for VMA coefficient q < VAR(p)
   Eigen::MatrixXd FullA = Eigen::MatrixXd::Zero(num_full_arows, dim); // same size with VMA coefficient matrix
   FullA.block(0, 0, month * dim, dim) = coef_mat.block(0, 0, month * dim, dim); // fill first mp row with VAR coefficient matrix
-  Eigen::MatrixXd Im = Eigen::MatrixXd::Identity(dim, dim); // identity matrix
+  // Eigen::MatrixXd Im = Eigen::MatrixXd::Identity(dim, dim); // identity matrix
   Eigen::MatrixXd ma = Eigen::MatrixXd::Zero(ma_rows, dim); // VMA [W1^T, W2^T, ..., W(lag_max)^T]^T, ma_rows = m * lag_max
-  ma.block(0, 0, dim, dim) = Im; // W0 = Im
-  ma.block(dim, 0, dim, dim) = FullA.block(0, 0, dim, dim) * ma.block(0, 0, dim, dim); // W1^T = B1^T * W1^T
-  if (lag_max == 1) return ma;
+  ma.topRows(dim) = Eigen::MatrixXd::Identity(dim, dim); // W0 = Im
+  ma.middleRows(dim, dim) = FullA.topRows(dim) * ma.topRows(dim); // W1^T = B1^T * W1^T
+  if (lag_max == 1) {
+		return ma;
+	}
   for (int i = 2; i < (lag_max + 1); i++) { // from W2: m-th row
     for (int k = 0; k < i; k++) {
-      ma.block(i * dim, 0, dim, dim) += FullA.block(k * dim, 0, dim, dim) * ma.block((i - k - 1) * dim, 0, dim, dim); // Wi = sum(W(i - k)^T * Bk^T)
+      ma.middleRows(i * dim, dim) += FullA.middleRows(k * dim, dim) * ma.middleRows((i - k - 1) * dim, dim); // Wi = sum(W(i - k)^T * Bk^T)
     }
   }
   return ma;
 }
 
-inline Eigen::MatrixXd convert_vhar_vma_ortho(Eigen::MatrixXd vhar_coef, Eigen::MatrixXd vhar_covmat, Eigen::MatrixXd HARtrans_mat, int lag_max, int month) {
+inline Eigen::MatrixXd convert_vhar_vma_ortho(Eigen::Ref<Eigen::MatrixXd> vhar_coef, Eigen::Ref<Eigen::MatrixXd> vhar_covmat, Eigen::Ref<Eigen::MatrixXd> HARtrans_mat, int lag_max, int month) {
   int dim = vhar_covmat.cols(); // num_rows = num_cols
   if ((dim != vhar_covmat.rows()) && (dim != vhar_coef.cols())) {
     STOP("Wrong covariance matrix format: `vhar_covmat`.");
@@ -151,25 +153,25 @@ inline Eigen::MatrixXd convert_vhar_vma_ortho(Eigen::MatrixXd vhar_coef, Eigen::
   Eigen::LLT<Eigen::MatrixXd> lltOfcovmat(Eigen::Map<Eigen::MatrixXd>(vhar_covmat.data(), dim, dim)); // cholesky decomposition for Sigma
   Eigen::MatrixXd chol_covmat = lltOfcovmat.matrixU();
   for (int i = 0; i < lag_max + 1; i++) {
-    res.block(i * dim, 0, dim, dim) = chol_covmat * ma.block(i * dim, 0, dim, dim);
+    res.middleRows(i * dim, dim) = chol_covmat * ma.middleRows(i * dim, dim);
   }
   return res;
 }
 
-inline Eigen::MatrixXd compute_vhar_mse(const Eigen::MatrixXd& cov_mat, const Eigen::MatrixXd& vhar_coef,
-																				const Eigen::MatrixXd& har_trans, int month, int step) {
+inline Eigen::MatrixXd compute_vhar_mse(Eigen::Ref<Eigen::MatrixXd> cov_mat, Eigen::Ref<Eigen::MatrixXd> vhar_coef,
+																				Eigen::Ref<Eigen::MatrixXd> har_trans, int month, int step) {
 	int dim = cov_mat.cols(); // dimension of time series
   Eigen::MatrixXd vma_mat = convert_vhar_to_vma(vhar_coef, har_trans, month, step);
   Eigen::MatrixXd mse(dim * step, dim);
   mse.topLeftCorner(dim, dim) = cov_mat; // sig(y) = sig
   for (int i = 1; i < step; i++) {
-    mse.block(i * dim, 0, dim, dim) = mse.block((i - 1) * dim, 0, dim, dim) + 
-      vma_mat.block(i * dim, 0, dim, dim).transpose() * cov_mat * vma_mat.block(i * dim, 0, dim, dim);
+    mse.middleRows(i * dim, dim) = mse.middleRows((i - 1) * dim, dim) + 
+      vma_mat.middleRows(i * dim, dim).transpose() * cov_mat * vma_mat.middleRows(i * dim, dim);
   }
   return mse;
 }
 
-inline Eigen::MatrixXd compute_vma_fevd(Eigen::MatrixXd vma_coef, Eigen::MatrixXd cov_mat, bool normalize) {
+inline Eigen::MatrixXd compute_vma_fevd(Eigen::Ref<Eigen::MatrixXd> vma_coef, Eigen::Ref<Eigen::MatrixXd> cov_mat, bool normalize) {
   int dim = cov_mat.cols();
   // Eigen::MatrixXd vma_mat = VARcoeftoVMA(var_coef, var_lag, step);
   int step = vma_coef.rows() / dim; // h-step

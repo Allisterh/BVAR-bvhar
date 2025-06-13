72f66c5ca2d679e9a2fb2e81c187475745f56c39
ygeunkim <ygeunkimstat@gmail.com>
2025-06-13 13:39:56 +0900

    remove && in old gcc

--- inst/include/bvhar/src/core/commondefs.h
+++ inst/include/bvhar/src/core/commondefs.h
@@ -117,14 +117,21 @@
 #if !defined(__cpp_lib_make_unique)
 namespace std {
 
+#if defined(__GNUC__) && (__GNUC__ <= 8)
+
+template <typename T, typename... Args>
+unique_ptr<T> make_unique(Args... args) {
+	return unique_ptr<T>(new T(args...));
+}
+
+#else
+
 template <typename T, typename... Args>
 unique_ptr<T> make_unique(Args&&... args) {
-// #if defined(__GNUC__) && (__GNUC__ <= 8)
-// 	return unique_ptr<T>(new T(forward<typename remove_reference<Args>::type>(args)...));
-// #else
 	return unique_ptr<T>(new T(forward<Args>(args)...));
-// #endif
 }
+
+#endif
 
 } // namespace std
 #endif
